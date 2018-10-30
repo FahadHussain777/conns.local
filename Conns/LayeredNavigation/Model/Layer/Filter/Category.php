ca6933a9e990c9bd7ecfec1d25c1f07d7407faa7
Fahad Hussain <root@pf2rf2.local>
2018-10-30 16:20:35 +0500

    lets start 2

--- Conns/LayeredNavigation/Model/Layer/Filter/Category.php
+++ Conns/LayeredNavigation/Model/Layer/Filter/Category.php
@@ -53,7 +53,6 @@ class Category extends \Magento\CatalogSearch\Model\Layer\Filter\Category
             return $this;
         }
         $productCollection = $this->getLayer()->getProductCollection();
-        $productCollection->getSelect();
         $this->applyToCollection($productCollection);
         $categoryCollection = ObjectManager::getInstance()->create(
             \Magento\Catalog\Model\ResourceModel\Category\Collection::class
