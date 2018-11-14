49e992d92c6f7c56cdc2c1814fe59ea72ad0411d
Fahad Hussain <root@pf2rf2.local>
2018-11-14 22:55:09 +0500

    compare product almost done

--- Conns/ProductCompare/Block/Main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Conns\ProductCompare\Block;
-
-class Main extends \Magento\Framework\View\Element\Template
-{
-    function _prepareLayout(){}
-}
---
+++ Conns/ProductCompare/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            'Magento_Catalog/js/view/compare-products':'Conns_ProductCompare/js/view/compare-products'
+        }
+    }
+};
