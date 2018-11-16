d36adeee950ff6c1d27845f8e1f4b4d4997a3776
Fahad Hussain <root@pf2rf2.local>
2018-11-16 15:34:46 +0500

    Layered Navigation renamed

--- Conns/LayeredNavigation/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Conns_LayeredNavigation',
-        __DIR__
-    );
